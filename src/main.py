68f9e8fbeebcb68aa749bee5e1d38932d46bf466
MarcoVilera <Marcovilarev123@gmail.com>
2025-04-25 21:33:12 -0400

    Refactor data handling and parsing for transportation and server allocation; update main function to include new solvers

--- src/main.py
+++ src/main.py
@@ -5,10 +5,22 @@ from data_handler import DataHandler
 
 def main():
     # Ejemplo de uso para el módulo Húngaro
-    data = DataHandler.read_file('data/input_hungarian.txt')
-    n, m, cost_matrix = DataHandler.parse_hungarian(data)
+    Hdata = DataHandler.read_file('../data/input_hungarian.txt')
+    cost_matrix = DataHandler.parse_hungarian(Hdata)
     hungarian = HungarianSolver(cost_matrix)
     assignments, cost = hungarian.solve()
     print(f"Asignaciones: {assignments}, Costo Total: {cost}")
+
+    Tdata = DataHandler.read_file('../data/input_transportation.txt')
+    cost_matrix, supply, demand = DataHandler.parse_transportation(Tdata)
+    transportation = TransportationSolver(cost_matrix, supply, demand)
+    initial_solution, total_cost = transportation.solve(method='northwest')
+    print(f"Solución Inicial: {initial_solution}, Costo Total: {total_cost}")
+
+    Sdata = DataHandler.read_file('../data/input_server.txt')
+    cost_matrix, capacities, priorities = DataHandler.parse_server_allocation(Sdata)
+    server_allocation = ServerAllocationSolver(cost_matrix, capacities, priorities)
+    assignments, total_time = server_allocation.solve()
+    print(f"Asignaciones: {assignments}, Tiempo Total: {total_time}")
 
 main()
